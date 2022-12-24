88841456783cf5fa89b25e71d92ed720bd53b1d9
joaccocaballero <joaquincaballero182@gmail.com>
2022-12-24 13:01:23 -0300

    final commit

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports = {
   production: {
-    "username": to_do_list_1o8j_user,
-    "password": p9jWOxkBZHRvmvasCX3Y70PAzDC1Sv1a,
-    "database": to_do_list_1o8j,
-    "host": dpg-cejhvasgqg4ekmd41ts0-a,
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "host": process.env.PGHOST,
     "dialect": "postgres"
   }
 }
